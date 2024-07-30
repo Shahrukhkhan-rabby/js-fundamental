7b75166e75ae2ea99a1162dbdb3293f56bd1ff00
Shahrukhkhan-rabby <shahrukhkhan3418198@gmail.com>
2024-07-30 20:41:58 +0600

    j

---
+++ conditional.js
@@ -0,0 +1,26 @@
+let isFoodReady = false;
+// jodi khabar ready hoi tailea ami khamu
+// if food is ready i will eat
+// if food is ready == true;
+if(isFoodReady == true) {
+    // console.log('Mama!! I will eat now.');
+}
+let iphonePrice = 70000;
+let myBudget = 9500;
+
+// if(iphonePrice < myBudget){
+//     console.log('possible');
+// }
+// if(iphonePrice > myBudget){
+//     console.log('I will happy with what I have');
+// }
+
+
+let chickenPrice = 180;
+let myMoney = 850;
+if (chickenPrice < myMoney) {
+    console.log('I will eat')
+}
+else {
+console.log('I will eat smashed potatoes')
+}
---
+++ multiple-condition.js
@@ -0,0 +1,8 @@
+var gotJob = false;
+
+if (gotJob == true) {
+    console.log('I will spend in the way of Allah more and more');
+}
+else {
+    console.log('still happay & Alhamhamdulillah!')
+}
