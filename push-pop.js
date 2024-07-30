19d71f6e00880933dd1c685d0e9ed253ae4559ce
Shahrukhkhan-rabby <shahrukhkhan3418198@gmail.com>
2024-07-30 18:50:46 +0600

    dm

---
+++ push-pop.js
@@ -0,0 +1,26 @@
+// add elements to an array
+var lastBench = ['kalam', 'balam', 'salam'];
+lastBench.push('jalam');
+lastBench.push('palam');
+lastBench.push('nalam');
+// console.log(lastBench);
+
+var friendsAge = [11, 13, 17, 12];
+friendsAge.push(19)
+// console.log(friendsAge);
+
+// remove element from an array
+friendsAge.pop();
+// console.log(friendsAge)
+lastBench.pop();
+
+// console.log(lastBench);
+
+var lastItem = friendsAge.pop();
+console.log(lastItem);
+
+// javascript remove element from beginning of array
+
+var array = [1, 2, 3, 4, 5,];
+var firstElement = array.shift();
+console.log(firstElement);
