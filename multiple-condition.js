0ca9e06240dc554e1aababe94881c45c4f6e4b24
Shahrukhkhan-rabby <shahrukhkhan3418198@gmail.com>
2024-07-31 11:35:05 +0600

    next

--- multiple-condition.js
+++ multiple-condition.js
@@ -1,6 +1,16 @@
-var gotJob = false;
+var gotJob = true;
+var moneySaved = 10000;
+var hasFlat = false;
+var hasHouse = true;
 
-if (gotJob == true) {
+
+// if (gotJob == true && moneySaved > 200000) {
+//     console.log('I will spend in the way of Allah more and more');
+// }
+// else {
+//     console.log('still happay & Alhamhamdulillah!')
+// }
+if ((gotJob == true && moneySaved > 200000) || hasFlat == true || hasHouse) {
     console.log('I will spend in the way of Allah more and more');
 }
 else {
