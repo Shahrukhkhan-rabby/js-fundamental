1b1aec239d71ae05c908ea729bd2313278cacfb8
Shahrukhkhan-rabby <shahrukhkhan3418198@gmail.com>
2024-07-31 12:21:35 +0600

    apatoto

---
+++ nested-condition.js
@@ -0,0 +1,31 @@
+var danishPrice = 50;
+var butterBreadPrice = 25;
+var toastBiscuitPrice = 10;
+var myBudget = 5;
+var packedWell = false
+
+
+
+// if (danishPrice < myBudget) {
+//     console.log('danishPrice < Danish kheyea danish jabo');
+// }
+// else if (butterBreadPrice < myBudget){
+//     console.log('Butter bon diyea cha khamu');
+//     }
+// else if (toastBiscuitPrice < myBudget){
+// console.log('toast biscuit khamu');
+// }
+
+// else{
+//     console.log('Batasha diyea cha khamu')
+
+// }
+
+if(danishPrice < myBudget){
+    if(packedWell == true){
+        console.log('danish khabo')
+    }
+    else{
+        console.log('danish khamu na! ')
+    }
+}
