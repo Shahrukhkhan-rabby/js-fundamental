f50b2a9cc1850f5ff3e31eef37843e06ca456cf3
Shahrukhkhan-rabby <shahrukhkhan3418198@gmail.com>
2024-07-31 13:18:26 +0600

    q

---
+++ summary.js
@@ -0,0 +1,36 @@
+var tableLength = 12;
+var tourDestinations = ['coxsbazar', 'nepal', 'vutan', 'paris' ];
+
+tourDestinations.indexOf('vutan');
+
+var fourDestination = tourDestinations[3];
+tourDestinations[1] = 'srilanka';
+
+tourDestinations.push('London');
+tourDestinations.pop();
+
+if(tourDestinations[1] == 'nepal'){
+    console.log('Spread the Message of Islam')
+}
+else if(tourDestinations[1] == 'china'){
+    console.log('here also I will invite people to Islam')
+}
+else if(tourDestinations.length == 4){
+    console.log('tell them to read their book')
+}
+else{
+    console.log('I will do Dhikr of Allah')
+}
+
+
+var eggPrice = 32;
+var myBudget = 32;
+if(eggPrice <= myBudget){
+    console.log('ami dim khamu');
+}
+if(eggPrice >= myBudget){
+    console.log('ami lebu diyea');
+}
+
+
+
